2caf18a5c2469860059953683b6a4c246da4640c
Ilham1606 <16ilhamapriansyah@gmail.com>
2023-02-05 09:19:07 +0700

    salah commit challenge pertemuan 2

--- challenge2.py
+++ challenge2.py
@@ -78,37 +78,4 @@ driver.close()
 
 
 
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
 
-# driver = webdriver.Chrome()
-# driver.get("https://demoqa.com/webtables")
-
-# wait = WebDriverWait(driver, 10)
-
-# # Hapus data yang ada
-# rows = wait.until(EC.presence_of_all_elements_located((By.XPATH, "//table[@id='table1']//tr")))
-# for row in rows[1:]:
-#     delete_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Delete']")))
-#     delete_button.click()
-#     confirm_delete = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Delete']")))
-#     confirm_delete.click()
-
-# # Tambahkan 3 data user baru
-# for i in range(3):
-#     add_user_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Add User']")))
-#     add_user_button.click()
-#     first_name_input = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@id='firstName']")))
-#     first_name_input.send_keys(f"First{i}")
-#     last_name_input = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@id='lastName']")))
-#     last_name_input.send_keys(f"Last{i}")
-#     user_email_input = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@id='userEmail']")))
-#     user_email_input.send_keys(f"user{i}@email.com")
-#     age_input = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@id='age']")))
-#     age_input.send_keys("30")
-#     submit_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[text()='Save']")))
-#     submit_button.click()
-
-# driver.quit()
